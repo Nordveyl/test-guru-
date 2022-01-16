4a89ad3012cf43bd0f9b3fb0c0d7e037106f4233
Nordveyl <kkrutoychel.wf@gmail.com>
2022-01-16 15:58:57 +0300

    3

--- db/migrate/20220114232543_create_tests.rb
+++ db/migrate/20220114232543_create_tests.rb
@@ -4,6 +4,7 @@ class CreateTests < ActiveRecord::Migration[7.0]
       t.string :title, null: false
       t.integer :level, null: false, default: 1
       t.references :category, null: false, foreign_key: true 
+      t.references :user, null: false, foreign_key: true 
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -39,14 +39,16 @@ ActiveRecord::Schema.define(version: 2022_01_14_235612) do
     t.string "title", null: false
     t.integer "level", default: 1, null: false
     t.integer "category_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
-    t.string "second_name", null: false
+    t.string "last_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -54,4 +56,5 @@ ActiveRecord::Schema.define(version: 2022_01_14_235612) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
