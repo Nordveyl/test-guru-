c30e570c7c4446c68208c5d3da3e7da89e0dc34d
Nordveyl <kkrutoychel.wf@gmail.com>
2022-01-18 16:46:38 +0300

    second commit

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
-	def self.sorted_test(category)
-	  Test 
-	  .joins('JOIN categories ON categories.id = tests.category_id')
-	  .where(categories: { name: category})
-	  .order('id DESC')
-	end  	
+  def self.sorted_test(category)
+    Test 
+    .joins('JOIN categories ON categories.id = tests.category_id')
+    .where(categories: { name: category})
+    .order('id DESC')
+  end   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   def show_passed_tests(level)
-  	Test 
-  	.joins('JOIN results ON rusults.test_id = tests.id')
-  	.where(results: {user_id: id}, level: level)
-  end 	  
+    Test 
+    .joins('JOIN results ON rusults.test_id = tests.id')
+    .where(results: {user_id: id}, level: level)
+  end     
 end
