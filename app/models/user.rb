82d4908996ee9f2dc9b909081ae6c3c2d597bd22
Nordveyl <kkrutoychel.wf@gmail.com>
2022-01-20 22:36:40 +0300

    4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :created_tests, class_name 'Test', dependent: :destroy
 
   def show_passed_tests(level)
-   tests.where(level: level)
+    tests.where(level: level)
   end     
 end
