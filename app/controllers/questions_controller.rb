8caf167681c24788266d9f920816a00cdb5c2d7e
Nordveyl <kkrutoychel.wf@gmail.com>
2022-02-01 07:14:19 +0300

    2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@ class QuestionsController < ApplicationController
   before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  
   def index 
     questions = @test.questions.pluck(:name)
     render plain: questions.join('  ')
